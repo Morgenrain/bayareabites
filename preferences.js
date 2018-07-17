de3ebf1cba48a983bca33da134b7f618580ea04f
Morgenrain <rainyyyan@gmail.com>
2018-07-17 14:10:53 -0700

    Initial commit

---
+++ preferences.js
@@ -0,0 +1,32 @@
+var preferences = []
+
+window.onload=hide();
+
+function hide(){
+  document.getElementById('meal').style.visibility = "hidden";
+  document.getElementById('mood').style.visibility = "hidden";
+  document.getElementById('prices').style.visibility = "hidden";
+  document.getElementById('submit').style.visibility = "hidden";
+}
+
+function showMeal(){
+  document.getElementById('meal').style.visibility = "visible";
+}
+
+function showMood(){
+  document.getElementById('mood').style.visibility = "visible";
+}
+
+function showPrices(){
+  document.getElementById('prices').style.visibility = "visible";
+}
+
+function showButton(){
+  document.getElementById('submit').style.visibility = "visible";
+}
+
+function save(){
+  document. {
+
+  }
+}
