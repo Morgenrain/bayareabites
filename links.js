76fba44545d1871eba43c8551caafa5471967901
avahooman <avahooman@gmail.com>
2018-07-23 13:51:30 -0700

    links

---
+++ links.js
@@ -0,0 +1,243 @@
+var str = "Onigilly Menu";
+var result = str.link("http://www.onigilly.com/menu-palo-alto/");
+
+var str = "Driftwood Deli And Market Menu";
+var result = str.link("http://driftwooddeliandmarket.com/menu-sandwiches.html");
+
+var str = "Pluto's Menu";
+var result = str.link("http://www.plutosfreshfood.com/menu.html");
+
+var str = "Zareen's Menu";
+var result = str.link("http://www.zareensrestaurant.com/food.html");
+
+var str = "Pokelove Menu";
+var result = str.link("http://pokelove.com/menu/");
+
+var str = "Ramen Nagi Menu";
+var result = str.link("https://www.tripadvisor.com/LocationPhotoDirectLink-g1758900-d5896009-i90226776-Ramen_Nagi-Taguig_City_Metro_Manila_Luzon.html");
+
+var str = "Evvia Estiatorio Menu";
+var result = str.link("https://evvia.net/wp-content/uploads/evvia-lunch-menu.pdf");
+
+var str = "Zola Menu";
+var result = str.link("http://www.zolapaloalto.com/menu");
+
+var str = "Tamarine Restaurant Menu";
+var result = str.link("https://www.yelp.com/menu/tamarine-restaurant-palo-alto/dinner-menu");
+
+var str = "Pampas Menu";
+var result = str.link("http://places.singleplatform.com/pampas-palo-alto/menu?ref=google");
+
+var str = "Cream Menu";
+var result = str.link("https://creamnation.com/menu/ice-cream-sandwich/");
+
+var str = "T4 Menu";
+var result = str.link("https://www.zomato.com/palo-alto-ca/t4-palo-alto/menu");
+
+var str = "Tony's Chicken Menu";
+var result = str.link("https://www.yelp.com/biz_photos/fast-tonys-chicken-palo-alto-2?tab=menu");
+
+var str = "Cafe Brioche Menu";
+var result = str.link("http://www.paloaltocafebrioche.com/menu.aspx");
+
+var str = "Joanie’s Cafe Menu";
+var result = str.link("http://www.joaniescafepaloalto.com/menus.html");
+
+var str = "Tootsies Menu";
+var result = str.link("https://www.zomato.com/palo-alto-ca/tootsies-palo-alto/menu");
+
+var str = "Pop Tea Bar Menu";
+var result = str.link("https://www.zomato.com/palo-alto-ca/pop-tea-bar-1-palo-alto/menu");
+
+var str = "Bare Bowls Menu";
+var result = str.link("https://www.barebowls.com/menu/");
+
+var str = "Chilly & Munch Menu";
+var result = str.link("https://chillyandmunch.com/");
+
+var str = "Teaspoon Menu";
+var result = str.link("https://www.teaspoonlife.com/menu");
+
+var str = "Earthbar Menu";
+var result = str.link("https://www.zomato.com/san-francisco/earthbar-cow-hollow/menu");
+
+
+var str = "Lazy Dog Restaurant & Bar Menu";
+var result = str.link("https://www.lazydogrestaurants.com/menuhttps://www.lazydogrestaurants.com/menu");
+
+var str = "Gochi Japanese Fusion Tapas Menu";
+var result = str.link("http://www.gochifusiontapas.com/menu.html");
+
+var str = "Galpão Gaucho Brazilian Steakhouse Menu";
+var result = str.link("https://galpaogauchousa.com/menu");
+
+var str = "Ike’s Love & Sandwiches Menu";
+var result = str.link("http://ilikeikesplace.com/");
+
+var str = "Red Hot Wok Menu";
+var result = str.link("https://www.zomato.com/cupertino-ca/red-hot-wok-cupertino/menu");
+
+var str = "Aqui Cal Mex Menu";
+var result = str.link("https://www.aquicalmex.com/cupertino-menu/");
+
+var str = "Kura Revolving Sushi Bar Menu";
+var result = str.link("https://kurausa.com/menu/");
+
+var str = "Yiassoo Menu";
+var result = str.link("https://work.grubhub.com/restaurant-menu-100254/yiassoo/");
+
+var str = "Philz Coffee Menu";
+var result = str.link("https://www.philzcoffee.com/menu");
+
+var str = "Vitality Bowls Menu";
+var result = str.link("https://vitalitybowls.com/");
+
+var str = "Fantasia Coffee & Tea Menu";
+var result = str.link("https://www.menuism.com/menus/fantasia-coffee-tea-66633#fantasia-coffee-tea-cupertino-130867");
+
+var str = "Zareen's Restaurant Menu";
+var result = str.link("https://www.zomato.com/mountain-view-ca/zareens-restaurant-mountain-view/menu");
+
+var str = "Icicles Menu";
+var result = str.link("http://iciclescreamroll.com/menu/");
+
+var str = "Plaza Deli Cafe Menu";
+var result = str.link("http://places.singleplatform.com/plaza-deli-cafe/menu");
+
+var str = "Asian Box Menu";
+var result = str.link("http://places.singleplatform.com/asian-box-1/menu");
+
+var str = "Roger's Deli Menu";
+var result = str.link("https://rogersdeli.eat24hour.com/menu");
+
+var str = "The Sandwich Spot Menu";
+var result = str.link("https://www.allmenus.com/ca/mountain-view/374971-the-sandwich-spot/menu/");
+
+var str = "Taqueria la Espuela Menu";
+var result = str.link("https://www.laespuelaca.com/menu.html");
+
+var str = "Maru Ichi Menu";
+var result = str.link("https://www.allmenus.com/ca/mountain-view/47282-maru-ichi-japanese-noodle-house/menu/");
+
+var str = "Sushi Tomi Menu";
+var result = str.link("https://www.yelp.com/menu/sushi-tomi-mountain-view-2");
+
+var str = "Cascal Menu";
+var result = str.link("https://cascalrestaurant.com/menu/");
+
+var str = "Vaso Azzurro Ristorante Menu";
+var result = str.link("http://www.vasoazzurro.com/menu-original");
+
+var str = "Sunny Bowl Menu";
+var result = str.link("http://places.singleplatform.com/sunny-bowl/menu");
+
+var str = "Crepevine Menu";
+var result = str.link("http://www.crepevine.com/wp-content/uploads/2017/09/Crepevine-Menu-Mountainview-March-2017-1.pdf");
+
+var str = "Bierhaus Menu";
+var result = str.link("https://www.zomato.com/mountain-view-ca/bierhaus-mountain-view/menu");
+
+var str = "In-N-Out Menu";
+var result = str.link("http://www.in-n-out.com/menu.aspx");
+
+var str = "Clarkes Charcoal Broiler Menu";
+var result = str.link("https://www.clarkes.com/menus.html");
+
+var str = "Mamacitas Tacos Menu";
+var result = str.link("https://www.mamacitas.com/menus/");
+
+var str = "Bajis Cafe Menu";
+var result = str.link("https://www.tripadvisor.com/Restaurant_Review-g32761-d923120-Reviews-Bajis_Cafe-Mountain_View_California.html");
+
+var str = "Dana Street Roasting Company Menu";
+var result = str.link("http://www.danastreetroasting.com/menu/");
+
+var str = "Din Ding Dumpling House Menu";
+var result = str.link("https://www.zmenu.com/din-ding-dumpling-house-fremont-online-menu/");
+
+var str = "Sweet Garden Menu";
+var result = str.link("https://www.menuwithprice.com/menu/sweet-garden/");
+
+var str = "Papillon Menu";
+var result = str.link("http://www.papillonrestaurant.com/menu.html");
+
+var str = "Little By Little Menu";
+var result = str.link("https://www.zmenu.com/little-by-little-fremont-2-online-menu/");
+
+var str = "Anikis Sushi Menu";
+var result = str.link("http://www.anikisushi.com/menu.html");
+
+var str = "Dish-N-Dash Menu";
+var result = str.link("https://www.yelp.com/menu/dish-n-dash-sunnyvale");
+
+var str = "Le Moose Crepe Cafe Menu";
+var result = str.link("https://menupages.com/le-moose-crepe-cafe/5014-mowry-ave-fremont");
+
+var str = "Soo Ja Fusion Bistro Menu";
+var result = str.link("https://www.soojafusionbistro.net/menus");
+
+var str = "Teo Chow Noodle Shack Menu";
+var result = str.link("https://foursquare.com/v/teo-chow-noodle-shack/4f3f203ee4b0c78cf8ccf865/menu");
+
+var str = "Momo Cook Menu";
+var result = str.link("https://www.menupix.com/menudirectory/menu.php?id=31015529");
+
+var str = "Bob Sang Korean BBQ and Tofu Menu";
+var result = str.link("https://www.zmenu.com/bob-sang-korean-bbq-and-tofu-fremont-online-menu/");
+
+var str = "Dog Haus Menu";
+var result = str.link("http://www.doghaus.com/menu.html");
+
+var str = "Picnic Sandwich Bistro Menu";
+var result = str.link("https://foursquare.com/v/picnic-sandwich-bistro/4cb8a6d69552b60c7260ce8b/menu");
+
+var str = "Frodo Joes Petit Cafe Menu";
+var result = str.link("https://www.allmenus.com/ca/fremont/392243-frodo-joes-petit-cafe/menu/");
+
+var str = "Federicos Grill Menu";
+var result = str.link("https://www.zmenu.com/federicos-grill-fremont-online-menu/");
+
+var str = "Bento Express Menu";
+var result = str.link("https://www.bentoxpress.com/menu.html");
+
+var str = "Dishdash Menu";
+var result = str.link("https://www.yelp.com/menu/dishdash-sunnyvale");
+
+var str = "St. Johns Bar and Grill Menu";
+var result = str.link("https://stjohnsgrill.com/menu/");
+
+var str = "Falafel Stop Menu";
+var result = str.link("http://www.falafelstop.com/menu");
+
+var str = "Bonchon Menu";
+var result = str.link("http://pc1t1altvxof3uw1gd8yvtow-wpengine.netdna-ssl.com/wp-content/uploads/2018/01/T2_take-out-menu_sunnyvale_optimized.pdf");
+
+var str = "Slice of New York Menu";
+var result = str.link("http://www.asliceofny.com/sunnyvale/menu/");
+
+var str = "Casa Lupe Number 2 Menu";
+var result = str.link("https://www.zomato.com/sunnyvale-ca/casa-lupe-sunnyvale/menu");
+
+var str = "Tasty Indian Pizza Menu";
+var result = str.link("http://www.tastyindianpizza.com/sunnyvale_menu.php");
+
+var str = "The Bay Fish and Chips Menu";
+var result = str.link("https://www.zomato.com/sunnyvale-ca/bay-fish-chips-sunnyvale/menu");
+
+var str = "Armadillo Willy's BBQ Menu";
+var result = str.link("http://www.armadillowillys.com/food.asp");
+
+var str = "PF Chang's Menu";
+var result = str.link("https://www.pfchangs.com/menu/menu-mobile");
+
+var str = "Poki Bowl Menu";
+var result = str.link("http://pokibowl.com/our-menu/");
+
+var str = "Jersey Mike's Subs Menu";
+var result = str.link("https://www.jerseymikes.com/menu");
+
+var str = "Hot Point Station Menu";
+var result = str.link("http://www.hotpointstation.com/en/bbq-menu/");
+
+var str = "El Sabroson de la Linea 2 Menu";
+var result = str.link("https://www.zmenu.com/el-sabroson-de-la-linea-2-sunnyvale-online-menu/");
